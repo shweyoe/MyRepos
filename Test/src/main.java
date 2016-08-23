7b4b6721ca211facc95ca30f069a91dd5ded9162
SLMO <slmo@gmail.com>
2016-08-23 18:27:10 +0700

    product and sum

--- Test/src/main.java
+++ Test/src/main.java
@@ -4,10 +4,12 @@ public class main {
 	public static void main(String[] args) {
 		int[] num={1,2,3,4,5};
 		int sum=0;
+		int product=0;
 		for(int i:num){
 			sum+=i;
+			product=sum*sum;
 		}
-		System.out.println("Total sum is= "+sum);
+		System.out.println("Total sum is= "+sum+"product is "+product);
 	}
 
 }
