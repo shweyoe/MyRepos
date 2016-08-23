41848ef3932028f5ffeb682b597491dfe264f9c7
other <other@gmail.com>
2016-08-23 18:01:55 +0700

    more efficient

--- Test/src/main.java
+++ Test/src/main.java
@@ -2,8 +2,9 @@
 public class main {
 
 	public static void main(String[] args) {
+		int[] num={1,2,3,4,5};
 		int sum=0;
-		for(int i=1;i<5;i++){
+		for(int i:num){
 			sum+=i;
 		}
 		System.out.println("Total sum is= "+sum);
