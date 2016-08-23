910b53caa1f950900c76788d0f4ce41e59391c58
SLMO <slmo@gmail.com>
2016-08-23 17:16:36 +0700

    Second check

---
+++ Test/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int[] num={1,2,3,4,5};
+		int sum=0;
+		for(int i:num){
+			sum+=i;
+		}
+		System.out.println("Total sum is"+sum);
+	}
+
+}
